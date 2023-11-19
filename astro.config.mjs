0498bfe40e2549242421b858aa7de80de0380640
Jack Englund <jackhenglund@yahoo.com>
2023-11-19 16:48:40 -0600

    install critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-
+import crittersSlim from "astro-critters-slim";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), crittersSlim()],
 	image: {
 		domains: ["files.bestplates.net"],
 		remotePatterns: [{ protocol: "https" }],
